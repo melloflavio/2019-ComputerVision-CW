81ebd248c5b46cb5232e04e679af9083eb556d73
Flavio Mello <flaviorafmello@gmail.com>
2019-03-25 20:56:53 +0000

    Separate IMG and file processing for face extracting

---
+++ Scripts/ExtractFaces.m
@@ -0,0 +1,21 @@
+function [faces] = ExtractFaces(imgPath)
+%EXTRACTFACES Extracts all faces from single image and returns the cropped
+%face images in a cell array
+%% Read image
+    imgObj = imread(imgPath);
+
+%% Detect Faces
+    faceDetector = vision.CascadeObjectDetector('ClassificationModel','FrontalFaceCART');
+    boundingBoxes = faceDetector(imgObj);
+
+%% Crop & return faces as a cell array
+    faceCount = size(boundingBoxes,1);
+    faces = cell(faceCount);
+    for bboxIdx = 1:faceCount
+        boundingBox = boundingBoxes(bboxIdx, :);
+        croppedFace = imcrop(imgObj, boundingBox);
+        faces{bboxIdx} = croppedFace;
+    end
+    
+end
+
---
+++ Scripts/ExtractFacesFromFolder.m
@@ -0,0 +1,33 @@
+function [] = ExtractFacesFromFolder(inFolder, outFolder)
+%EXTRACTFACESFROMFOLDER Extract all faces from all .jpg images from source
+%folder, writing the cropped face images to destination folder. Image
+%search is non recursive (i.e. 1 folder deep only)
+%   @inFolder: Folder containing images to have faces extracted
+%   @outFolder: Folder which should contain all detination images
+%   This function leverages ExtractFaces which processes the image &
+%   returns the faces. This keeps the image processing and file managing
+%   layers separate & independent
+
+    %% Process in/out paths
+    allImages = dir(fullfile(inFolder, "*.jpg")); % Get images from inFolder
+    mkdir(outFolder); % Ensure outfolder exists to avoid permission issues with imwrite
+
+    %% Iterate over images extracting & saving faces
+    for imageIdx = 1:size(allImages,1)
+        % Get path components
+        imageFolder = allImages(imageIdx).folder;
+        imageName = allImages(imageIdx).name;
+        imagePath = fullfile(imageFolder, imageName);
+        [~, imageNamePlain, ~] = fileparts(imagePath); % Get name without extension
+        % Extract faces
+        faces = ExtractFaces(imagePath);
+
+        % Save Faces to destination folder
+        for faceIdx = 1:size(faces,1)
+            faceImg = faces{faceIdx};
+            outFilename = strcat(imageNamePlain, "__f_", int2str(faceIdx), ".jpg");
+            outFilepath = fullfile(outFolder, outFilename);
+            imwrite(faceImg, outFilepath);
+        end
+    end
+end
