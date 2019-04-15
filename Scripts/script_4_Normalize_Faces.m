9a0290df5265430ef23a54873cd54abf0369feac
Flavio Mello <flaviorafmello@gmail.com>
2019-04-15 23:12:44 +0100

    Extract image normalization to separate function

--- Scripts/script_4_Normalize_Faces.m
+++ Scripts/script_4_Normalize_Faces.m
@@ -3,10 +3,6 @@ inputFolder = "processed_4";
 outputFolder = "processed_5";
 inputBasepath = fullfile("../Dataset", inputFolder);
 outputBasepath= fullfile("../Dataset", outputFolder);
-individualPicFolderName = "individual_pic";
-individualMovFolderName = "individual_mov";
-groupPicFolderName = "group_pic";
-groupMovFolderName = "group_mov";
 
 %% Normalization Feature Toggle
 shouldAlignEyes = false;
@@ -25,37 +21,11 @@ numSets = size(imgSetCollection, 2);
 for setIdx=1:numSets
     currentSet = imgSetCollection(setIdx);
     for imgIdx = 1:imgSetCollection(setIdx).Count
+        % Read image
         currentImage = read(currentSet, imgIdx);
         
-        % Rotate Image to level both eyes
-        if(shouldAlignEyes)
-            leftEyeDetector = vision.CascadeObjectDetector('ClassificationModel','LeftEye');
-            rightEyeDetector = vision.CascadeObjectDetector('ClassificationModel','RightEye');
-            leftEyeBox = leftEyeDetector(currentImage);
-            rightEyeBox = rightEyeDetector(currentImage);
-            
-            % Perform rotation only if both eyes were found. Otherwise keep
-            % image as is
-            if (~isempty(leftEyeBox) && ~isempty(rightEyeBox))
-                [leftEye_x, leftEye_y] = CalculateBBoxCenter(leftEyeBox);
-                [rightEye_x, rightEye_y] = CalculateBBoxCenter(rightEyeBox);
-
-                delta_w = rightEye_x - leftEye_x;
-                delta_h = rightEye_y - leftEye_y;
-
-                theta = atand(delta_h/delta_w);
-
-                currentImage = imrotate(currentImage, -theta, 'bicubic', 'crop');
-            end
-        end
-        % Convert to grayscale
-        if (shouldConvertToGrayscale)
-            currentImage = rgb2gray(currentImage);
-        end
-        % Resize image
-        if (shouldResizeImages)
-            currentImage = imresize(currentImage, outputImageSize, resizeInterpolationMethod);
-        end
+        % Normalize image
+        currentImage = NormalizeImage(currentImage);
         
         % Output processed image
         % Write processed image to next folder while keep the exact same 
