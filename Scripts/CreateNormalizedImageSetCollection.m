76823f52b6bd82c719c9fac00c9d8bfa00329b42
Flavio Mello <flaviorafmello@gmail.com>
2019-04-15 17:10:51 +0100

    Update CreateNormalizedImageSetCollection.m

--- Scripts/CreateNormalizedImageSetCollection.m
+++ Scripts/CreateNormalizedImageSetCollection.m
@@ -11,14 +11,14 @@ function [imageSetCollection] = CreateNormalizedImageSetCollection(parentFolder)
 %CreateImageSetFromSubfolders
 
 %% Get all folders representing each individual
-individualFolders = ListSubfolders(inputBasepath);
+individualFolders = ListSubfolders(parentFolder);
 
 %% Iterate over individual people folders creating individual image sets
 imageSetCollection = [];
 for folderIdx = 1:size(individualFolders,2)
     folderName = individualFolders{folderIdx};
     
-    individualImgSet = CreateImageSetFromSubfolders(fullfile(inputBasepath, folderName), folderName);
+    individualImgSet = CreateImageSetFromSubfolders(fullfile(parentFolder, folderName), folderName);
 %     fprintf('Building ImageSet for folder %s with %d images\n', folderName, individualImgSet.Count);
     % Store imageset in array containing image sets for each person
     imageSetCollection = [imageSetCollection, individualImgSet];
