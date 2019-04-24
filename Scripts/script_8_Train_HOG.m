6e29ee30ecae3bb1979f20dff715db559bc346fc
Flavio Mello <flaviorafmello@gmail.com>
2019-04-24 23:11:57 +0100

    Update HOG to include KNN & print prediction time

--- Scripts/script_8_Train_HOG.m
+++ Scripts/script_8_Train_HOG.m
@@ -10,12 +10,14 @@ testFeatures = ExtractHogFeatures(testImgDs, hogCellSize);
 %% Naive Bayes -  NB
 disp("Training NB...");
 tic;
-hogNb = fitcnb(trainFeatures, trainImgDs.Labels);
+hogNb = fitcnb(trainFeatures, trainImgDs.Labels, 'DistributionNames', 'kernel');
 execTime = toc;
 fprintf("Trained in %f seconds...\n", execTime);
 
 % Test
+tic;
 predictedNb = predict(hogNb, testFeatures);
+fprintf("Predicted in %f seconds...\n", toc);
 confNb = confusionmat(testImgDs.Labels, predictedNb);
 accuracyNb = sum(testImgDs.Labels == predictedNb)/size(predictedNb, 1);
 fprintf('Accuracy: %d\n', accuracyNb);
@@ -23,6 +25,28 @@ fprintf('Accuracy: %d\n', accuracyNb);
 % Save & clear to free memory
 save("../results/NB_HOG_CONF", 'confNb');
 save("../models/NB_HOG", 'hogNb', '-v7.3');
+clear('hogNb');
+
+%% K-Nearest Neighbours - KNN
+disp("Training KNN...");
+tic;
+hogKnn = fitcknn(trainFeatures, trainImgDs.Labels, 'NumNeighbors',5 , 'Standardize', 1);
+% fitcknn(X,Y,'NumNeighbors',5,'Standardize',1)
+execTime = toc;
+fprintf("Trained in %f seconds...\n", execTime);
+
+% Test
+tic;
+predictedKnn = predict(hogKnn, testFeatures);
+fprintf("Predicted in %f seconds...\n", toc);
+confKnn = confusionmat(testImgDs.Labels, predictedKnn);
+accuracyKnn = sum(testImgDs.Labels == predictedKnn)/size(predictedKnn, 1);
+fprintf('Accuracy: %d\n', accuracyKnn);
+
+% Save & clear to free memory
+save("../results/KNN_HOG_CONF", 'confKnn');
+save("../models/KNN_HOG", 'hogKnn', '-v7.3');
+clear('hogKnn');
 
 %% Support Vector Machine - SVM
 disp("Training SVM...");
@@ -33,7 +57,9 @@ execTime = toc;
 fprintf("Trained in %f seconds...\n", execTime);
 
 % Test
+tic;
 predictedSvm = predict(hogSvm, testFeatures);
+fprintf("Predicted in %f seconds...\n", toc);
 confSvm = confusionmat(testImgDs.Labels, predictedSvm);
 accuracySvm = sum(testImgDs.Labels == predictedSvm)/size(predictedSvm, 1);
 fprintf('Accuracy: %d\n', accuracySvm);
@@ -41,7 +67,7 @@ fprintf('Accuracy: %d\n', accuracySvm);
 % Save & clear to free memory
 save("../results/SVM_HOG_CONF", 'confSvm');
 save("../models/SVM_HOG", 'hogSvm', '-v7.3');
-clear(hogSvm);
+clear('hogSvm');
 
 %% Random Forest - RF
 %Train
@@ -53,7 +79,9 @@ execTime = toc;
 fprintf("Trained in %f seconds...\n", execTime);
 
 % Test
+tic;
 predictedRf = predict(hogRf, testFeatures);
+fprintf("Predicted in %f seconds...\n", toc);
 predictedRf = categorical(predictedRf);
 confRf = confusionmat(testImgDs.Labels, predictedRf);
 accuracyRf = sum(testImgDs.Labels == predictedRf)/size(predictedRf, 1);
@@ -62,4 +90,4 @@ fprintf('Accuracy: %d\n', accuracyRf);
 % Save & clear to free memory
 save("../results/RF_HOG_CONF", 'confRf');
 save("../models/RF_HOG", 'hogRf', '-v7.3');
-clear(hogRf);
+clear('hogRf');
