6e90fce50ed80b2d63943869f50659e90d694c2c
Flavio Mello <flaviorafmello@gmail.com>
2019-04-26 00:56:49 +0100

    Fixes in recognizeFace

--- Scripts/CalculateBBoxCenter.m
+++ Scripts/CalculateBBoxCenter.m
@@ -4,5 +4,9 @@ function [x_center, y_center] = CalculateBBoxCenter(boundingBox)
 
     x_center = boundingBox(1) + (boundingBox(3)/2); % center = left bound + (width/2)
     y_center = boundingBox(2) + (boundingBox(4)/2); % center = upper bound + (height/2)
+    
+    % Pixel values are integer
+    x_center = round(x_center);
+    y_center = round(y_center);
 end
 
